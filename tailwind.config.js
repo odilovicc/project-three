d5d044cdfed9f34357d1e56a4c08ecec19440829
Bakhtiyor <odilovskijarik@gmail.com>
2023-07-30 12:39:01 +0500

    Consulting + delivering section (responsive)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: '0 0 25px rgba(129, 45, 226, 1), 0 0 50px rgba(129, 45, 226, 1)', // Используйте ваш пользовательский цвет здесь
+      },
+    },
   },
+  variants: {},
   plugins: [],
-}
-
+};
